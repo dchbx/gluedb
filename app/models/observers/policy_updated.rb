6ec0c803ffbacc7e0bcf96a624b06b77e1dfd19e
Nikhil S <nikhilks219@gmail.com>
2020-04-06 12:24:28 -0400

    86567: 1095 triggers - 12/31/19 term initiated by DC should not trigger a corrected form

--- app/models/observers/policy_updated.rb
+++ app/models/observers/policy_updated.rb
@@ -5,6 +5,37 @@ module Observers
       notify_of_federal_reporting_changes(policy, time)
     end
 
+    def self.check_for_npt_flag_end_date(policy, before_updated_policy)
+      policy.term_for_np == before_updated_policy.term_for_np && before_updated_policy.policy_end.nil?
+    end
+
+    def self.check_for_termination_date(policy)
+      if policy.policy_end.present? && policy.policy_end == policy.policy_end.try(:end_of_year)
+        if policy.versions.present?
+          before_updated_policy = policy.versions.order_by(updated_at: :desc).first
+          check_for_npt_flag_end_date(policy, before_updated_policy) && check_dependent_end_date(policy, before_updated_policy)
+        end
+      end
+    end
+
+    def self.check_dependent_end_date(policy, before_updated_policy)
+      policy.enrollees.each do |enrollee|
+        unless enrollee.rel_code == "self"
+          before_updated_policy.enrollees.each do |previous_enrollee|
+            unless previous_enrollee.rel_code == "self"
+              if enrollee.id == previous_enrollee.id
+                # it return false only when
+                # previous enrollee coverage_end date is not an end of that year
+                # current version enrollee coverage_end is equal to end of that year
+                return false if previous_enrollee.coverage_end != previous_enrollee.coverage_end.try(:end_of_year) && enrollee.coverage_end == enrollee.coverage_end.try(:end_of_year)
+              end
+            end
+          end
+        end
+      end
+      true
+    end
+
     def self.notify_of_federal_reporting_changes(policy, time)
       # Coverall policies are are not considered part of IVL marketplace
       return if policy.kind == 'coverall'
@@ -12,6 +43,7 @@ module Observers
       return if policy.coverage_type.to_s.downcase != "health"
       return if policy.coverage_year.first.year == time.year
       return if policy.coverage_year.first.year < 2018
+      return if check_for_termination_date(policy)
       ::Amqp::EventBroadcaster.with_broadcaster do |b|
         b.broadcast(
           {
--- spec/models/observers/policy_updated_spec.rb
+++ spec/models/observers/policy_updated_spec.rb
@@ -82,6 +82,111 @@ describe Observers::PolicyUpdated do
     end
   end
 
+  describe 'given IVL policy terminated at 12/31/PY' do
+    let!(:child)   {
+      person = FactoryGirl.create :person, dob: Date.new(1998, 9, 6)
+      person.update(authority_member_id: person.members.first.hbx_member_id)
+      person
+    }
+    let!(:plan) { FactoryGirl.create(:plan) }
+    let!(:primary) {
+      person = FactoryGirl.create :person, dob: Date.new(1970, 5, 1)
+      person.update(authority_member_id: person.members.first.hbx_member_id)
+      person
+    }
+    let(:coverage_start) {Date.new(2019, 1, 1)}
+    let(:coverage_end) {coverage_start.end_of_year}
+    let(:policy) {
+      policy = FactoryGirl.create :policy, plan_id: plan.id, coverage_start: coverage_start, coverage_end: nil, kind: 'individual'
+      policy.enrollees[0].m_id = primary.authority_member.hbx_member_id
+      policy.enrollees[1].m_id = child.authority_member.hbx_member_id
+      policy.enrollees[1].rel_code ='child'; policy.save
+      policy
+    }
+    let(:policy_id) {policy.id}
+    let(:eg_id) {policy.eg_id}
+    let(:event_broadcaster) do
+      instance_double(Amqp::EventBroadcaster)
+    end
+
+    before(:each) do
+      allow(Amqp::EventBroadcaster).to receive(:with_broadcaster).and_yield(event_broadcaster)
+    end
+
+    context 'when policy passes check_for_npt_flag_end_date and check_dependent_end_date' do
+
+      it "does nothing" do
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        policy.terminate_as_of(coverage_end)
+        expect(event_broadcaster).not_to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'when depedent has coverage end date other then 12/31/PY' do
+      let(:policy2) {
+        policy = FactoryGirl.create :policy, plan_id: plan.id, coverage_start: coverage_start, coverage_end: nil, kind: 'individual'
+        policy.enrollees[0].m_id = primary.authority_member.hbx_member_id
+        policy.enrollees[1].m_id = child.authority_member.hbx_member_id
+        policy.enrollees[1].rel_code ='child'
+        policy.enrollees[1].update_attributes!(coverage_end: Time.mktime(2019, 8, 31)); policy.save
+        policy
+      }
+      let(:policy2_id) {policy2.id}
+      let(:eg_id2) {policy2.eg_id}
+
+      it "sends the message" do
+        expect(policy2.is_shop?).to eq false
+        expect(policy2.coverage_type).to eq "health"
+        policy2.terminate_as_of(coverage_end)
+        policy2.enrollees[1].update_attributes!(coverage_end: Time.mktime(2019, 12, 31))
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy2_id,
+              :eg_id => eg_id2,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy2, today)
+      end
+    end
+
+    context 'when term_for_np for current version and previous version is not same' do
+
+      it "sends the message" do
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        policy.terminate_as_of(coverage_end)
+        policy.update_attributes!(term_for_np: true)
+        policy.save!
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+  end
+
   context "given an ivl policy from a previous policy year, after 1/1/2018" do
     let(:policy_id) { "A POLICY ID" }
     let(:eg_id) { "A POLICY ID" }
@@ -98,6 +203,7 @@ describe Observers::PolicyUpdated do
         is_shop?: false,
         kind: 'individual',
         coverage_year: coverage_year_first,
+        policy_end: Time.mktime(2018, 11, 30),
         coverage_type: "health"
       )
     end
