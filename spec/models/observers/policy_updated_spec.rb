a66830717beae532500765a87678603b07b1368f
Nikhil S <nikhilks219@gmail.com>
2020-04-07 12:15:35 -0400

    86567: adding few other test scenarios

--- spec/models/observers/policy_updated_spec.rb
+++ spec/models/observers/policy_updated_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Observers::PolicyUpdated do
+describe Observers::PolicyUpdated, :dbclean => :after_each do
   let(:today) { Time.mktime(2025, 1, 1) }
   let(:current_year) { ((today.beginning_of_year)..today.end_of_year) }
   let(:coverage_year_first) { (Time.mktime(2018, 1, 1)..Time.mktime(2018, 12, 31) )}
@@ -82,7 +82,7 @@ describe Observers::PolicyUpdated do
     end
   end
 
-  describe 'given IVL policy terminated at 12/31/PY' do
+  describe 'given IVL policy is terminated', :dbclean => :after_each do
     let!(:child)   {
       person = FactoryGirl.create :person, dob: Date.new(1998, 9, 6)
       person.update(authority_member_id: person.members.first.hbx_member_id)
@@ -113,7 +113,7 @@ describe Observers::PolicyUpdated do
       allow(Amqp::EventBroadcaster).to receive(:with_broadcaster).and_yield(event_broadcaster)
     end
 
-    context 'when policy passes check_for_npt_flag_end_date and check_dependent_end_date' do
+    context 'given IVL policy is terminated at 12/31/PY' do
 
       it "does nothing" do
         expect(policy.is_shop?).to eq false
@@ -133,7 +133,72 @@ describe Observers::PolicyUpdated do
       end
     end
 
-    context 'when depedent has coverage end date other then 12/31/PY' do
+    context 'when there is no IVL policy versions present' do
+
+      it "sends the message" do
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        policy.subscriber.update_attributes!(coverage_end: "12/31/2019")
+        policy.save!
+        expect(policy.versions.present?).to eq false
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'given IVL policy is canceled' do
+
+      it "sends the message" do
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        policy.terminate_as_of(coverage_start)
+        expect(policy.aasm_state).to eq 'canceled'
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'when term_for_np for current version and previous version is not same' do
+
+      it "sends the message" do
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        policy.terminate_as_of(coverage_end)
+        policy.update_attributes!(term_for_np: true)
+        policy.save!
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'when dependent has coverage end date other then 12/31/PY' do
       let(:policy2) {
         policy = FactoryGirl.create :policy, plan_id: plan.id, coverage_start: coverage_start, coverage_end: nil, kind: 'individual'
         policy.enrollees[0].m_id = primary.authority_member.hbx_member_id
@@ -164,14 +229,16 @@ describe Observers::PolicyUpdated do
       end
     end
 
-    context 'when term_for_np for current version and previous version is not same' do
-
+    context 'when premium amount updated' do
       it "sends the message" do
+        policy.enrollees.where(rel_code: "self").first.update_attributes!(coverage_end: coverage_end)
+        policy.save
+        expect(policy.pre_amt_tot.to_f).to eq 666.66
         expect(policy.is_shop?).to eq false
         expect(policy.coverage_type).to eq "health"
-        policy.terminate_as_of(coverage_end)
-        policy.update_attributes!(term_for_np: true)
-        policy.save!
+        policy.update_attributes!(pre_amt_tot: "600.11")
+        policy.save
+        expect(policy.pre_amt_tot.to_f).to eq 600.11
         expect(event_broadcaster).to receive(:broadcast).with(
           {
             :headers => {
@@ -183,6 +250,68 @@ describe Observers::PolicyUpdated do
           ""
         )
         Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'when dropping a dependent' do
+      it "sends the message" do
+        policy.enrollees.where(rel_code: "self").first.update_attributes!(coverage_end: coverage_end)
+        policy.save
+        expect(policy.is_shop?).to eq false
+        expect(policy.coverage_type).to eq "health"
+        expect(policy.enrollees.count).to eq 2
+        policy.enrollees.where(rel_code: "child").first.destroy
+        policy.save!
+        expect(policy.enrollees.count).to eq 1
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy_id,
+              :eg_id => eg_id,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy, today)
+      end
+    end
+
+    context 'when adding a dependent' do
+      let!(:policy3) {
+        policy = FactoryGirl.create :policy, plan_id: plan.id, coverage_start: coverage_start, coverage_end: coverage_end, kind: 'individual'
+        policy.enrollees[0].m_id = primary.authority_member.hbx_member_id
+        policy.enrollees[1].m_id = child.authority_member.hbx_member_id
+        policy.enrollees[1].rel_code ='child'; policy.save
+        policy
+      }
+      let!(:spouse)   {
+        person = FactoryGirl.create :person, dob: Date.new(1981, 9, 6)
+        person.update(authority_member_id: person.members.first.hbx_member_id)
+        person
+      }
+      let(:policy3_id) {policy3.id}
+      let(:eg_id3) {policy3.eg_id}
+
+      it "sends the message" do
+        expect(policy3.is_shop?).to eq false
+        expect(policy3.coverage_type).to eq "health"
+        expect(policy3.enrollees.count).to eq 2
+        new_dependent = policy3.enrollees.create!(rel_code: "spouse", m_id: spouse.authority_member_id, coverage_start: "2/1/2019", coverage_end: "12/31/2019")
+        new_dependent.save
+        policy3.save!
+        expect(policy3.enrollees.count).to eq 3
+        expect(event_broadcaster).to receive(:broadcast).with(
+          {
+            :headers => {
+              :policy_id => policy3_id,
+              :eg_id => eg_id3,
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+        Observers::PolicyUpdated.notify(policy3, today)
       end
     end
   end
